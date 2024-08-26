4a540ddcd6d68aa0b44c3a8087bce72472d4b4ce
Alex Iftime <alecs.iftime@gmail.com>
2024-08-26 18:19:02 +0300

    am adaugat MapTheory

--- src/main/java/org/example/dsa/MapTheory.java
+++ src/main/java/org/example/dsa/MapTheory.java
@@ -1,89 +1,81 @@
 package org.example.dsa;
 
 /*
-    Set = este o structura de date care stocheaza colectii de elemente distincte
-    Daca se va incerca intr-un set sa se introduc elemente identice, mereu se va pastra prima aparitie.
+    Map = este o structura de date care permite stocarea elementelor de tipul pereche
+    Perechile sunt de forma (cheie, valoare)
+    Cheile reprezinta un subset al Mapei constituent de valori unice.
+    Valoarile reprezinta o sublista a Mapei costituenta din valori de orice fel (si unice, si duplicitatare)
 
-    In set nu mai exista conceptul de index, ci de iterator.
-
-    Setul este la baza o interfata care e implementata de catre HashSet.
-
-    !!!!!!!!
-    Vizibilitatea unui obiect in cod este restrictionata de zona de definitie.
-    Daca definim spre exemplu un obiect in interiorul unei instructiuni conditionale/repetitive, acesta
-    va avea posibilitatea de accesare doar in contextul metodei, si nu in afara ei.
-
-    Daca definim un obiect in metoda PSVM la linia 49, acest obiect va fi accesibil incepand cu linia 50. Nu inainte!
-    !!!!!!!!
+    Implementarea finala a interfetei Map este HashMap fiind foarte similara ca si reguli de functionare cu HashSet.
  */
 
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapTheory {
 
     public static void main(String[] args) {
-        /* Initializare si definire a unui set */
-        Set<String> myHashSet = new HashSet<>();
 
-        /* ADD & ADD ALL */
-        myHashSet.add("apple"); // true, valoarea nu exista in set, asa ca a fost adaugata
-        myHashSet.add("pear");
-        myHashSet.add("cherry");
-        System.out.println(myHashSet);
+        /* Initializarea si definirea unei mape */
+        Map<String, Integer> myHashMap = new HashMap<>();
 
-        myHashSet.add("apple"); // false, setul a ramas neschimbat
-        System.out.println(myHashSet);
+        /* PUT = simetricul din List/Set al lui ADD */
+        // putem face atat adaugare de pereche, cat si actualizare
+        myHashMap.put("John", 25);
+        myHashMap.put("Alice", 12);
+        myHashMap.put("Matthew", 87);
+        System.out.println(myHashMap);
+        System.out.println("------------------------");
 
-        myHashSet.addAll(new HashSet<>(Set.of("apple", "banana"))); // se adauga doar elementele care nu exista
-        // daca folosim ca si parametru un set, se va face uniunea celor 2
-        // prin concatenarea setului original la cel dat ca parametru
-        System.out.println(myHashSet);
-        System.out.println("-----------------------------");
+        /* PUT ALL = putem face atat adaugare de perechi, cat si actualizare */
+        myHashMap.putAll(new HashMap<>(
+                Map.of(
+                        "Matei", 56,
+                        "Bob", 99,
+                        "John", 123
+                )
+        ));
+        System.out.println(myHashMap);
+        System.out.println("------------------------");
+
+        /* PUT IF ABSENT = putem face adaugare, doar daca cheia nu exista */
+        // daca exista, nu facem actualizare
+        myHashMap.putIfAbsent("Laur", 66);
+        myHashMap.putIfAbsent("John", 66);
+        System.out.println(myHashMap);
+        System.out.println("------------------------");
+
+        /* GET */
+        System.out.println(myHashMap.get("Bob"));
+        System.out.println("------------------------");
+
+        /* VALUES/KEYSET */
+        System.out.println(myHashMap.values()); // extrage sublista de valori
+        System.out.println(myHashMap.keySet()); // extrage subsetul de chei
+        System.out.println("------------------------");
 
         /* ITERATION METHODS */
 
-        // enhanced for (functioneaza la orice structura de date: List, Set, Map..)
-        // nu avem control asupra indexului, putem face operatiuni exclusiv pe elementul curent reprezentat de iterator
-        System.out.print("Enhanced for example: ");
-        for(String it: myHashSet) {
+        // Enhanced for - keySet
+        System.out.println("Enhanced for keySet example: ");
+        for (String it : myHashMap.keySet()) {
+            System.out.println("(Key: " + it + ", Value: " + myHashMap.get(it) + ") ");
+        }
+        System.out.println("------------------------");
+
+        // Enhanced for - values
+        System.out.print("Enhanced for values example: ");
+        for(Integer it: myHashMap.values()) {
             System.out.print(it + " ");
         }
-        System.out.println();
+        System.out.println("\n------------------------");
 
-        // iterator object using while (functioneaza la orice structura de date...)
-        System.out.print("Iterator while example: ");
-        Iterator<String> it = myHashSet.iterator();
-        while(it.hasNext()) {
-            System.out.print(it.next() + " ");
+        // Pair iteration
+        System.out.println("Pair iteration: ");
+        for(Map.Entry<String, Integer> it: myHashMap.entrySet()) {
+            System.out.println("(Key: " + it.getKey() + ", Value: " + it.getValue() + ") ");
         }
-        System.out.println("\n-----------------------------");
 
-        /* REMOVE */
-        System.out.println(myHashSet.remove("apple") + " -> " + myHashSet);
-        System.out.println(myHashSet.remove("apple") + " -> " + myHashSet);
-        // todo: removeAll
-        System.out.println("\n-----------------------------");
-
-
-        /* CONTAINS */
-        System.out.println(myHashSet.contains("apple"));
-        System.out.println(myHashSet.contains("cherry"));
-        // todo: containsAll
-        System.out.println("\n-----------------------------");
-
-        // todo: size, clear, isEmpty
-
-        /* List to Set */
-        // Elimina toate elementele duplicitare dintr-o lista.
-        List<Integer> numbersList = new ArrayList<>(List.of(1,5,1,11,5,5,6,32,1));
-
-        // Varianta babeasca
-        // todo: creati o noua lista pe baza iteratiei celei originale in care verificati conditional
-        //  cu un if daca lista noua CONTINE valoarea curenta din for
-
-        // Varianta smart
-        Set<Integer> uniqueNumbersList = new HashSet<>(numbersList);
-        System.out.println(uniqueNumbersList);
+        // todo: iteration using iterator of Pair, remove, contains, clear, isEmpty
     }
 }
